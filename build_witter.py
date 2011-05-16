b6e1ec42c46a22383cfb122ad1fa1e938fe91ab4
jon <jon@chase.(none)>
2011-05-16 02:26:26 +0100

    update build file and perms

--- build_witter.py
+++ build_witter.py
@@ -51,14 +51,15 @@ fi
 if [ -f ~/.witterUser ]; then
   sed "/last_id\|last_dm\|last_men/d" ~/.witterUser > ~/.tmp_user
   mv ~/.tmp_user ~/.witterUser
+  chown user:users ~/.witterUser
 else
   :
 fi
     """
     version = "0.3.8"           #Version of your software, e.g. "1.2.0" or "0.8.2"
-    build = "12"                 #Build number, e.g. "1" for the first build of this version of your software. Increment for later re-builds of the same version of your software.
+    build = "17"                 #Build number, e.g. "1" for the first build of this version of your software. Increment for later re-builds of the same version of your software.
                                 #Text with changelog information to be displayed in the package "Details" tab of the Maemo Application Manager
-    changeloginformation = "Update python-twitter to 0.8.3 and change GetReplies to GetMentions newer api method" 
+    changeloginformation = "make sure witter.py has correct permissions" 
     dir_name = "src"            #Name of the subfolder containing your package source files (e.g. usr\share\icons\hicolor\scalable\myappicon.svg, usr\lib\myapp\somelib.py). We suggest to leave it named src in all projects and will refer to that in the wiki article on maemo.org
     #Thanks to DareTheHair from talk.maemo.org for this snippet that recursively builds the file list 
     for root, dirs, files in os.walk(dir_name):
