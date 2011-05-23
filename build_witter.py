a5c2aee6701df7f76b2247f35a091f51fa4c0484
jon <jon@chase.(none)>
2011-05-23 22:16:50 +0100

    update build file

--- build_witter.py
+++ build_witter.py
@@ -57,9 +57,9 @@ else
 fi
     """
     version = "0.3.8"           #Version of your software, e.g. "1.2.0" or "0.8.2"
-    build = "17"                 #Build number, e.g. "1" for the first build of this version of your software. Increment for later re-builds of the same version of your software.
+    build = "18"                 #Build number, e.g. "1" for the first build of this version of your software. Increment for later re-builds of the same version of your software.
                                 #Text with changelog information to be displayed in the package "Details" tab of the Maemo Application Manager
-    changeloginformation = "make sure witter.py has correct permissions" 
+    changeloginformation = "Remove +200 button due to changes in api, add in missing icon to Ayro theme" 
     dir_name = "src"            #Name of the subfolder containing your package source files (e.g. usr\share\icons\hicolor\scalable\myappicon.svg, usr\lib\myapp\somelib.py). We suggest to leave it named src in all projects and will refer to that in the wiki article on maemo.org
     #Thanks to DareTheHair from talk.maemo.org for this snippet that recursively builds the file list 
     for root, dirs, files in os.walk(dir_name):
